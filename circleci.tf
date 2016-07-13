b8cd435e05faa3db1379b55780ab5a596bd2bf46
Kevin Bell <kevin.a.bell@gmail.com>
2016-07-13 13:34:17 -0700

    Make bucket name valid and unique(ish)

--- circleci.tf
+++ circleci.tf
@@ -106,7 +106,9 @@ EOF
 # Single general-purpose bucket
 
 resource "aws_s3_bucket" "circleci_bucket" {
-    bucket = "${var.prefix}_bucket"
+    # VPC ID is used here to make bucket name globally unique(ish) while
+    # uuid/ignore_changes have some lingering issues
+    bucket = "${replace(var.prefix, "_", "-")}-bucket-${replace(var.aws_vpc_id, "vpc-", "")}"
     cors_rule {
         allowed_methods = ["GET"]
         allowed_origins = ["*"]
