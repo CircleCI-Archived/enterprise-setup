1941702a29b5ea6a9d768b020dc821fca8cb7291
iynere <iynere@users.noreply.github.com>
2018-02-06 12:10:14 -0500

    generate bats test

---
+++ test/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "#!/usr/bin/env bats"
+
+for i in $(cat /Users/avena/Desktop/amilist.txt);
+do
+  REGION=$(echo $i | cut -d "=" -f 1)
+  AMI_ID=$(echo $i | cut -d "=" -f 2)
+
+  cat << EOF
+  @test "\$REGION" {
+    printf "Validating \$AMI_ID located in \$REGION..."
+
+    aws --region $REGION ec2 describe-images --filters "Name=image-id,Values=\$AMI_ID" | jq '.Images[] | length'
+  }
+EOF
+
+done
