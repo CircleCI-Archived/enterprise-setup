e1a8e448664b8f9bbab89ffe7259227de47e156a
Chad Horohoe <39928225+chadasapp@users.noreply.github.com>
2018-11-15 11:20:41 -0800

    Vary output based on var.enabled

--- modules/nomad/outputs.tf
+++ modules/nomad/outputs.tf
@@ -1,15 +1,15 @@
 output "asg_name" {
-  value = "${aws_autoscaling_group.clients_asg.*.name}"
+  value = "${var.enabled ? aws_autoscaling_group.clients_asg.*.name[0] : ""}"
 }
 
 output "client_security_group_name" {
-  value = "${aws_security_group.nomad_sg.*.name}"
+  value = "${var.enabled ? aws_security_group.nomad_sg.*.name[0] : ""}"
 }
 
 output "ssh_security_group_name" {
-  value = "${aws_security_group.ssh_sg.*.name}"
+  value = "${var.enabled ? aws_security_group.ssh_sg.*.name[0] : ""}"
 }
 
 output "client_launch_config_name" {
-  value = "${aws_launch_configuration.clients_lc.*.name}"
+  value = "${var.enabled ? aws_launch_configuration.clients_lc.*.name[0] : ""}"
 }
