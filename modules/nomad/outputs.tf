716625e2e34b6c7511a5fd0dbfd82b3bf0bb1831
Noboru Kurumai <noboru@circleci.com>
2019-01-11 11:40:51 +0900

    fix terraform warnings

--- modules/nomad/outputs.tf
+++ modules/nomad/outputs.tf
@@ -1,15 +1,15 @@
 output "asg_name" {
-  value = "${aws_autoscaling_group.clients_asg.name}"
+  value = "${aws_autoscaling_group.clients_asg.*.name}"
 }
 
 output "client_security_group_name" {
-  value = "${aws_security_group.nomad_sg.name}"
+  value = "${aws_security_group.nomad_sg.*.name}"
 }
 
 output "ssh_security_group_name" {
-  value = "${aws_security_group.ssh_sg.name}"
+  value = "${aws_security_group.ssh_sg.*.name}"
 }
 
 output "client_launch_config_name" {
-  value = "${aws_launch_configuration.clients_lc.name}"
+  value = "${aws_launch_configuration.clients_lc.*.name}"
 }
